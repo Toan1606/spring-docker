9dacc07871a510b782cf393c177025983fc41299
codehunter <nguyenthetoan1606@gmail.com>
2022-06-23 11:02:10 +0700

    add service, repository testing

--- src/test/java/com/codedecode/demo/repository/PostingRepositoryTest.java
+++ src/test/java/com/codedecode/demo/repository/PostingRepositoryTest.java
@@ -1,5 +1,21 @@
 package com.codedecode.demo.repository;
 
-public class PostingRepositoryTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class PostingRepositoryTest {
+	
+	@Autowired
+	private PostingRepository postingRepository;
+	
+	@Test
+	public void testGetAllJob() {
+		assertNotNull(postingRepository);
+		assertEquals(postingRepository, postingRepository);
+	}
 }
--- src/test/java/com/codedecode/demo/service/PostingServiceTest.java
+++ src/test/java/com/codedecode/demo/service/PostingServiceTest.java
@@ -1,5 +1,28 @@
 package com.codedecode.demo.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.codedecode.demo.entity.Posting;
+import com.codedecode.demo.repository.PostingRepository;
+
 public class PostingServiceTest {
 
+	@MockBean
+	private PostingRepository postingRepository;
+	
+	@InjectMocks
+	private PostingService postingService;
+	
+	
+	@Test
+	public void testGetUrgentJob() {
+		when(postingRepository.getAllJob()).thenReturn(null);
+		Iterable<Posting> urgentJobs = postingService.getUrgentJob();
+		assertEquals(urgentJobs, null);
+	}
 }
