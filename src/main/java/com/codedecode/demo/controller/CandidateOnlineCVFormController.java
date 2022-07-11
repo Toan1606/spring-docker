d6407a4cf2cc66e1ffdfeb2b9bb104707cb9c9e0
codehunter <nguyenthetoan1606@gmail.com>
2022-07-11 23:33:18 +0700

    update spring security api

--- src/main/java/com/codedecode/demo/controller/CandidateOnlineCVFormController.java
+++ src/main/java/com/codedecode/demo/controller/CandidateOnlineCVFormController.java
@@ -8,14 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codedecode.demo.dto.CandidateOnlineFormDTO;
-import com.codedecode.demo.entity.Salary;
 import com.codedecode.demo.entity.User;
 import com.codedecode.demo.entity.WorkingForm;
 import com.codedecode.demo.entity.YearOfExperience;
--- src/main/java/com/codedecode/demo/controller/UserController.java
+++ src/main/java/com/codedecode/demo/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.codedecode.demo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.codedecode.demo.entity.Language;
 import com.codedecode.demo.entity.User;
 import com.codedecode.demo.entity.API.DataAPI;
 import com.codedecode.demo.entity.API.ErrorAPI;
