4119ee30a0df403b3be8fc852597b1cbc8e88467
codehunter <nguyenthetoan1606@gmail.com>
2022-07-29 12:11:52 +0700

    change mysql to mssql configuration

--- src/main/java/com/codedecode/demo/controller/RecruiterLoginController.java
+++ src/main/java/com/codedecode/demo/controller/RecruiterLoginController.java
@@ -1,7 +1,5 @@
 package com.codedecode.demo.controller;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,12 +10,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.codedecode.demo.dto.LoginRequestDTO;
-import com.codedecode.demo.dto.LoginResponseDTO;
 import com.codedecode.demo.dto.RegisterRequestDTO;
 import com.codedecode.demo.entity.User;
 import com.codedecode.demo.service.AuthService;
-import com.codedecode.demo.service.UserService;
+
 
 @RestController
 @RequestMapping("/recruiter")
@@ -27,8 +23,7 @@ public class RecruiterLoginController {
 	@Autowired
 	private AuthService authService;
 	
-	@Autowired
-	private UserService userService;
+
 	
 	/*
 	 * 
@@ -40,31 +35,5 @@ public class RecruiterLoginController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(registerRequestDTO));
 	}
 	
-	/*
-	 * 
-	 *	@author: Nguyen The Toan
-	 * 
-	 */
-//	@PostMapping(value = "/login")
-//	public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequest, HttpServletResponse response) {
-////		LoginResponseDTO loginResponse = authService.checkLogin(loginRequest);
-//		String email = loginRequest.getEmail();
-//		User loginUser = userService.getUserByEmail(email);
-//		Cookie cookie = new Cookie("refresh_token", loginResponse.getAccessToken().getToken());
-//		cookie.setMaxAge(3600);
-//		cookie.setHttpOnly(true);
-////		cookie.setPath("/login");
-//		response.addCookie(cookie);
-//		
-//		response.setHeader("Authorization", loginResponse.getAccessToken().getToken());
-//		return ResponseEntity.status(HttpStatus.OK).body(loginResponse);
-//	}
-	
-//	@PostMapping(value = "/decode")
-//	public ResponseEntity<Token> decodeToken(@RequestBody TokenResponse token) {
-//		String tokenCharacter = token.getToken();
-//		String secretKey = token.getSecretKey();
-//		System.out.println(Jwts.parser().setSigningKey(secretKey).parseClaimsJws(tokenCharacter).getBody());
-//		return new ResponseEntity<Token>(HttpStatus.OK);
-//	}
+
 }
