d73cf731b294f925cfaf5bdda40f77f2c942c94b
codehunter <nguyenthetoan1606@gmail.com>
2022-08-10 16:20:07 +0700

    add applied candidate

---
+++ src/main/java/com/codedecode/demo/dto/RecruiterAppliedJobResponseDTO.java
@@ -0,0 +1,30 @@
+package com.codedecode.demo.dto;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecruiterAppliedJobResponseDTO implements Serializable {/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long candidateId;
+	
+	private String candidateName;
+	
+	private Long postingId;
+	
+	private String postingPosition;
+	
+	private String appliedDate;
+}
